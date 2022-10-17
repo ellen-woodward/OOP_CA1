39a7ff6372da05ce660a33ae9e18453153e29efd
Ellen Woodward <S00222321@atu.ie>
2022-10-17 10:05:05 +0100

    Add project files.

---
+++ Q1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Enter birth year: ");
+int year = int.Parse(Console.ReadLine());
+
+int age = 2022 - year;
+
+if (age >= 18)
+    Console.WriteLine("You can vote!");
+else
+    Console.WriteLine("You cannot vote");
