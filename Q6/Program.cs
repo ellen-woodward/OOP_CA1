6967d1fef3a678438435cac050c07f654ee1153d
Ellen Woodward <S00222321@atu.ie>
2022-10-17 11:15:28 +0100

    Q6

---
+++ Q6/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Ellen Woodward (S00222321)
+ * 17/10/22
+ * CA1
+ */
+using Q6; //links classes and program.cs
+
+Worker worker1 = new Worker("Tom", "Plumber", 15); //instantiate new object Worker 
+Worker worker2 = new Worker("Jo", "Electrician", 20); //pass in info on new Worker
+
+List<Worker> workers = new List<Worker>(); //create list of type Worker
+workers.Add(worker1);
+workers.Add(worker2);
+
+DisplayInformation(workers);
+
+void DisplayInformation(List<Worker> workers)
+{
+    for (int i = 0; i < workers.Count; i++)
+    {
+        Console.WriteLine($"Worker {i+1}\nName : {workers[i].Name}\nType : {workers[i].Type}\nHourly rate : {workers[i].HourlyRate}\n");
+    }
+}
---
+++ Q6/Worker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q6
+{
+    internal class Worker
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public decimal HourlyRate { get; set; }
+
+        public Worker(string name, string type, decimal hourlyRate)
+        {
+            Name = name;
+            Type = type;
+            HourlyRate = hourlyRate;
+        }
+    }
+}
