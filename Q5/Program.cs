4635966ef081c64e3518f256664a28d37116c9be
Ellen Woodward <S00222321@atu.ie>
2022-10-17 11:00:49 +0100

    Q5

---
+++ Q5/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Ellen Woodward (S00222321)
+ * 17/10/22
+ * CA1
+ */
+
+double salary = 50000, inflation = 0.1;
+
+Console.WriteLine($"Original salary : {salary}");
+
+IncreaseSalaryByInflation(ref salary, inflation); //pass salary by reference to method
+
+Console.WriteLine($"Increased salary : {salary}");
+
+void IncreaseSalaryByInflation(ref double salary, double inflation)
+{
+    salary += salary * (inflation); //calculates new salary, which changes varaible outside of method
+}
